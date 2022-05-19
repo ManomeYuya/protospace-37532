639f6bda850d831344fe3e974e45cbdeaffd48a8
manomeyuya <98867182+ManomeYuya@users.noreply.github.com>
2022-05-19 22:07:27 +0900

    commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,19 @@
 class PrototypesController < ApplicationController
   def index
+    @prototypes = Prototypes.all
   end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:image,:title,:catch_copy,:concept).merge(user_id: current_user.id)
+  end
+
 end
